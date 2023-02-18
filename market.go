0004123b573358616b8f761741f339d7398aee9d
Ammar Bandukwala <ammar@ammar.io>
2023-02-18 12:51:21 -0600

    Finish market endpoints

--- market.go
+++ market.go
@@ -17,26 +17,63 @@ type Event struct {
 	Title             string    `json:"title"`
 }
 
-// SeriesResponse is described here:
+// EventsResponse is described here:
 // https://trading-api.readme.io/reference/getevents.
-type SeriesResponse struct {
+type EventsResponse struct {
 	CursorResponse
 	Events []Event `json:"events"`
 }
 
-// SeriesRequest is described here:
+// EventsRequest is described here:
 // https://trading-api.readme.io/reference/getevents.
-type SeriesRequest struct {
+type EventsRequest struct {
 	CursorRequest
 	// Status is one of "open", "closed", or "settled".
 	Status       string `url:"status,omitempty"`
 	SeriesTicker string `url:"series_ticker,omitempty"`
 }
 
-// GetSeries is described here:
+// Events is described here:
 // https://trading-api.readme.io/reference/getevents.
-func (c *Client) GetSeries(ctx context.Context, req SeriesRequest) {
+func (c *Client) Events(ctx context.Context, req EventsRequest) (*EventsResponse, error) {
+	var resp EventsResponse
 
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     "events",
+		QueryParams:  req,
+		JSONRequest:  nil,
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+// EventResponse is described here:
+// https://trading-api.readme.io/reference/getevent.
+type EventResponse struct {
+	Event   Event    `json:"event"`
+	Markets []Market `json:"markets"`
+}
+
+// GetEvent is described here:
+// https://trading-api.readme.io/reference/getevent.
+func (c *Client) GetEvent(ctx context.Context, event string) (*EventResponse, error) {
+	var resp EventResponse
+
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     "events/" + event,
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
 }
 
 // GetMarketsRequest is described here:
@@ -93,15 +130,126 @@ type MarketsResponse struct {
 // https://trading-api.readme.io/reference/getmarkets.
 func (c *Client) Markets(
 	ctx context.Context,
-	opts GetMarketsRequest,
+	req GetMarketsRequest,
 ) (*MarketsResponse, error) {
 	var resp MarketsResponse
 
 	err := c.request(ctx, request{
 		Method:       "GET",
 		Endpoint:     "markets",
-		QueryParams:  opts,
+		QueryParams:  req,
 		JSONRequest:  nil,
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+type Trade struct {
+	Count       int       `json:"count"`
+	CreatedTime time.Time `json:"created_time"`
+	NoPrice     int       `json:"no_price"`
+	TakerSide   string    `json:"taker_side"`
+	Ticker      string    `json:"ticker"`
+	TradeID     string    `json:"trade_id"`
+	YesPrice    int       `json:"yes_price"`
+}
+
+// TradesResponse is described here:
+// https://trading-api.readme.io/reference/gettrades.
+type TradesResponse struct {
+	CursorResponse
+	Trades []Trade `json:"trades,omitempty"`
+}
+
+// TradesRequest is described here:
+// https://trading-api.readme.io/reference/gettrades.
+type TradesRequest struct {
+	CursorRequest
+	Trades []Trade `json:"trades"`
+}
+
+// Trades is described here:
+// https://trading-api.readme.io/reference/gettrades.
+func (c *Client) Trades(
+	ctx context.Context,
+	req TradesRequest,
+) (*TradesResponse, error) {
+	var resp TradesResponse
+
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     "markets/trades",
+		QueryParams:  req,
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+// Market is described here:
+// https://trading-api.readme.io/reference/getmarket.
+func (c *Client) Market(ctx context.Context, ticker string) (*Market, error) {
+	var resp struct {
+		Market Market `json:"market"`
+	}
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     fmt.Sprintf("markets/%s", ticker),
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Market, nil
+}
+
+// MarketHistory is described here:
+// https://trading-api.readme.io/reference/getmarkethistory.
+type MarketHistory struct {
+	NoAsk        int       `json:"no_ask"`
+	NoBid        int       `json:"no_bid"`
+	OpenInterest int       `json:"open_interest"`
+	Ts           timestamp `json:"ts"`
+	Volume       int       `json:"volume"`
+	YesAsk       int       `json:"yes_ask"`
+	YesBid       int       `json:"yes_bid"`
+	YesPrice     int       `json:"yes_price"`
+}
+
+// MarketHistoryResponse is described here:
+// https://trading-api.readme.io/reference/getmarkethistory.
+type MarketHistoryResponse struct {
+	CursorResponse
+	History []MarketHistory `json:"history"`
+	Ticker  string          `json:"ticker"`
+}
+
+// MarketHistoryRequest is described here:
+// https://trading-api.readme.io/reference/getmarkethistory.
+type MarketHistoryRequest struct {
+	CursorRequest
+	MinTS timestamp `json:"min_ts,omitempty"`
+	MaxTS timestamp `json:"max_ts,omitempty"`
+}
+
+func (c *Client) MarketHistory(
+	ctx context.Context,
+	ticker string,
+	req MarketHistoryRequest,
+) (*MarketHistoryResponse, error) {
+	var resp MarketHistoryResponse
+
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     fmt.Sprintf("markets/%s/history", ticker),
+		QueryParams:  req,
 		JSONResponse: &resp,
 	})
 	if err != nil {
@@ -126,4 +274,29 @@ func (c *Client) MarketOrderBook(ctx context.Context, ticker string) (*OrderBook
 		return nil, err
 	}
 	return &resp.OrderBook, nil
+}
+
+// Series is described here:
+// https://trading-api.readme.io/reference/getseries.
+type Series struct {
+	Frequency string `json:"frequency"`
+	Ticker    string `json:"ticker"`
+	Title     string `json:"title"`
+}
+
+// Series is described here:
+// https://trading-api.readme.io/reference/getseries.
+func (c *Client) Series(ctx context.Context, seriesTicker string) (*Series, error) {
+	var resp struct {
+		Series Series `json:"series"`
+	}
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     fmt.Sprintf("series/%s", seriesTicker),
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Series, nil
 }
