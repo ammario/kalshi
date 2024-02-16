2b9ac9f235829a1b513c352d715ddf1643f0a61e
r4stl1n <royal.r4stl1n@gmail.com>
2024-02-15 23:46:11 -0800

    added exchange schedule endpoint

--- exchange.go
+++ exchange.go
@@ -9,6 +9,47 @@ type ExchangeStatusResponse struct {
 	TradingActive  bool `json:"trading_active,omitempty"`
 }
 
+// ExchangeScheduleResponse is described here:
+// https://trading-api.readme.io/reference/getexchangeschedule.
+type ExchangeScheduleResponse struct {
+	Schedule struct {
+		StandardHours struct {
+			Monday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"monday"`
+			Tuesday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"tuesday"`
+			Wednesday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"wednesday"`
+			Thursday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"thursday"`
+			Friday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"friday"`
+			Saturday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"saturday"`
+			Sunday struct {
+				OpenTime  string `json:"open_time"`
+				CloseTime string `json:"close_time"`
+			} `json:"sunday"`
+		} `json:"standard_hours"`
+		MaintenanceWindows []struct {
+			EndDatetime   string `json:"end_datetime"`
+			StartDatetime string `json:"start_datetime"`
+		} `json:"maintenance_windows,omitempty"`
+	} `json:"schedule"`
+}
+
 // ExchangeStatus is described here:
 // https://trading-api.readme.io/reference/getexchangestatus.
 func (c *Client) ExchangeStatus(ctx context.Context) (*ExchangeStatusResponse, error) {
@@ -16,6 +57,21 @@ func (c *Client) ExchangeStatus(ctx context.Context) (*ExchangeStatusResponse, e
 	err := c.request(ctx, request{
 		Method:       "GET",
 		Endpoint:     "exchange/status",
+		JSONResponse: &resp,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
+
+// ExchangeSchedule is described here:
+// https://trading-api.readme.io/reference/getexchangeschedule.
+func (c *Client) ExchangeSchedule(ctx context.Context) (*ExchangeScheduleResponse, error) {
+	var resp ExchangeScheduleResponse
+	err := c.request(ctx, request{
+		Method:       "GET",
+		Endpoint:     "exchange/schedule",
 		JSONResponse: &resp,
 	})
 	if err != nil {
